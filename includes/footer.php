5e62400ccdf64cbeeff9b93ad853ed9bc38a741c
Het9126 <136975740+Het9126@users.noreply.github.com>
2024-07-27 17:30:13 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Coppright &copy; NexCart | All Rights Reserved
+                Contact Us: +91 00000 00000|</b></span>
+    </div>
+</footer>
